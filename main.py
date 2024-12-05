205ee88d6df568746b7f94130aa318e40a31acf4
miraclemie <47073793+miraclemie@users.noreply.github.com>
2024-12-05 21:24:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ user_token = ""
 def root(token: str):
     if token != user_token:
         return {}
-    content = requests.get(mikan_url, proxies=proxies, timeout=30).content
+    content = requests.get(mikan_url).content
     doc = etree.fromstring(content)
 
     links = doc.xpath("//enclosure")
@@ -37,6 +37,34 @@ def root(token: str):
             f"{host_name}/get?token={user_token}&link=https://mikanani.me",
         )
         link.set("url", url)
+    
+    # 修改pubdate格式
+    namespaces = {'ns': 'https://mikanani.me/0.1/'}
+    for item in doc.findall('.//item'):
+        # 使用find找到torrent节点，再找到pubDate
+        pub_date_element = item.find('.//ns:torrent/ns:pubDate', namespaces)
+        if pub_date_element is not None:
+            try:  # 为了防止mikan忽然改格式导致InternalError
+                # 原始pubDate字符串
+                pub_date_str = pub_date_element.text
+                # 去掉微秒部分，转换格式
+                if '.' in pub_date_str:
+                    pub_date_str = pub_date_str.split('.')[0]  # 去掉微秒部分
+                # 解析原始日期字符串
+                pub_date = datetime.fromisoformat(pub_date_str)
+                # 将日期格式化为目标格式
+                formatted_pub_date = pub_date.strftime('%a, %d %b %Y %H:%M:%S +0800')
+                # 更新pubDate元素的文本
+                # pub_date_element.text = formatted_pub_date  # 也没必要
+
+                # 将pubDate元素移动到item下
+                # 创建新的pubDate节点
+                new_pub_date_element = etree.Element('pubDate')
+                new_pub_date_element.text = formatted_pub_date
+                # 将新的pubDate添加到item的最后
+                item.append(new_pub_date_element)
+            except:
+                continue
 
     content = etree.tostring(doc)
     return Response(content, media_type="application/xml")
@@ -46,9 +74,10 @@ def root(token: str):
 def get(token: str, link: str):
     if token != user_token:
         return {}
-    response = requests.get(link, proxies=proxies, timeout=30)
+    response = requests.get(link)
     media_type = response.headers.get("Content-Type")
     return Response(response.content, media_type=media_type)
+
 
 
 if __name__ == "__main__":
